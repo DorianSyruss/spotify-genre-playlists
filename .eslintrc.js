5ba31aadf0af4ac3a8cb1c03fe4338824220ddbf
Dorian Syruss <sharke.dev@gmail.com>
2022-02-15 11:43:09 +0100

    Add eslint config files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "@extensionengine",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: ['require-sort'],
+  rules: {
+    'require-sort/require-sort': ['error', {
+      ignoreCase: true,
+      propertySyntaxSortOrder: ['multiple', 'single', 'none']
+    }]
+  }
+};
